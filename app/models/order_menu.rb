6f1ef6178e5c7c17908616488d4b77ab48ee0741
Sachnun Kusmayadi <sachnun12@gmail.com>
2022-04-20 18:29:07 +0700

    update order menu model

--- app/models/order_menu.rb
+++ app/models/order_menu.rb
@@ -1,4 +1,10 @@
 class OrderMenu < ApplicationRecord
+    validates :porsi, numericality: { greater_than: 0 }
+
     belongs_to :order
-    belongs_to :menu
+    belongs_to :menu 
+
+    def self.total_harga
+        return sum(:harga)
+    end
 end
