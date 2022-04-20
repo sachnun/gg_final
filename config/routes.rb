b9b79a0169d2d7c63817ca845aff43d54512c82a
Sachnun Kusmayadi <sachnun12@gmail.com>
2022-04-20 18:29:17 +0700

    update routes api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  
+  scope :api do
+    resources :menus, only: [:index, :show, :create, :update, :destroy]
+    resources :kategoris, only: [:index]
+    resources :orders, only: [:index, :show, :create, :update, :destroy]
+  end  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: redirect('/api/menus')
 end
